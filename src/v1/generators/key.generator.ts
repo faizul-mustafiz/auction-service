68aa2e7e1ac07311d8e5ebaaea94571dbc0cb62d
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-04-21 03:24:58 +0600

    Add log file and key generator methods

---
+++ src/v1/generators/key.generator.ts
@@ -0,0 +1,13 @@
+import * as _crypto from 'crypto';
+const accessToken = _crypto.randomBytes(64).toString('hex');
+const refreshToken = _crypto.randomBytes(64).toString('hex');
+const verifyToken = _crypto.randomBytes(32).toString('hex');
+const changePasswordToken = _crypto.randomBytes(32).toString('hex');
+const resetPasswordToken = _crypto.randomBytes(32).toString('hex');
+console.table({
+  accessToken,
+  refreshToken,
+  verifyToken,
+  changePasswordToken,
+  resetPasswordToken,
+});
