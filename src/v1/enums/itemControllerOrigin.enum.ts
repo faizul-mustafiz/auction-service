4fe5c97ac35eb8ff96be4e16705c5875a708fa66
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-05-05 20:58:18 +0600

    Add items related enums

---
+++ src/v1/enums/itemControllerOrigin.enum.ts
@@ -0,0 +1,9 @@
+export enum ItemControllerOrigin {
+  getAllItems = 'getAllItems-base-error:',
+  getOneItem = 'getOneItem-base-error',
+  createOneItem = 'createOneItem-base-error:',
+  updateOneItem = 'updateOneItem-base-error:',
+  deleteOneItem = 'deleteOneItem-base-error:',
+  searchItem = 'searchItem-base-error:',
+  publishItem = 'publishItem-base-error:',
+}
